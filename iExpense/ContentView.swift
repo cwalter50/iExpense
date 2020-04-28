6b3e7ba4d946cbf24d06d80cecabb8550c583526
Chris Walter <walt50@gmail.com>
2020-04-27 21:52:11 -0400

    MVP Done

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,12 +8,50 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
-    }
-}
 
+struct ContentView: View {
+
+    @ObservedObject var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                   HStack {
+                       VStack(alignment: .leading) {
+                           Text(item.name)
+                               .font(.headline)
+                           Text(item.type)
+                       }
+
+                       Spacer()
+                       Text("$\(item.amount)")
+                   }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(trailing:
+                Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                }
+            )
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: self.expenses)
+            }
+            
+       }
+        
+   }
+}
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ iExpense/ExpenseItem.swift
@@ -0,0 +1,40 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Christopher Walter on 4/27/20.
+//  Copyright © 2020 Christopher Walter. All rights reserved.
+//
+
+import Foundation
+
+struct ExpenseItem: Identifiable, Codable {
+    let id = UUID()
+    let name: String
+    let type: String
+    let amount: Int
+}
+
+
+class Expenses: ObservableObject {
+    @Published var items: [ExpenseItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
+        }
+    }
+    
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "Items") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([ExpenseItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+
+        self.items = []
+    }
+}
